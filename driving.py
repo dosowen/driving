570829fb35cc31074ae154f043116d7425681670
dosowen <s103394@cmes.tn.edu.tw>
2018-06-10 11:15:24 +0800

    America

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
 country = input ('what country do you belong:')
-age = ('how old are you:')
+age = input ('how old are you:')
 age = int (age)
-if country == Taiwan:
+if country == 'Taiwan':
 	if age >= 18:
-		print ('you can have a drivers license')
+		print ("you can have a driver's license")
 	else:
-		print ('you cannot have a drivers license')
+		print ("you cannot have a driver's license")
+if country == 'America':
+	if age >= 16:
+		print ("you can have a driver's license")
+	else:
+		print ("you cannot have a driver's license")
